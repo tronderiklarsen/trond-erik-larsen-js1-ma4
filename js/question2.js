7c7f9c27ce72f10cf9f707960aa8e4b2682180a6
Trond <56018387+tronderiklarsen@users.noreply.github.com>
2020-02-12 19:14:05 +0100

    //2 finished

---
+++ js/question2.js
@@ -0,0 +1,31 @@
+const gamesUrl = "https://api.rawg.io/api/games";
+
+function handleResponse(response) {
+    return response.json();
+}
+
+function createGames(json) {
+    const results = json.results;
+
+    const container = document.querySelector(".results");
+    let html = "";
+
+    results.forEach(function(results) {
+        html += `<div class="game">
+                    <h2>${results.name}</h2>
+                    <img src="${results.background_image}" alt="${results.name}">
+                </div>`;
+    });
+
+container.innerHTML = html;
+
+}
+
+function handleError(error) {
+    console.log(error);
+}
+
+fetch(gamesUrl)
+    .then(handleResponse)
+    .then(createGames)
+    .catch(handleError)
