a8ca632be655b2e7a0a9dc614591b558bcf6cf00
Trond <56018387+tronderiklarsen@users.noreply.github.com>
2020-02-13 14:21:04 +0100

    finished //3

--- js/question3.js
+++ js/question3.js
@@ -1,12 +1,22 @@
 const gamesUrl = "https://api.rawg.io/api/games/4200";
 
 
-function handleRespons(response){
+
+function handleResponse(response){
     return response.json();
 }
 
 function createGameDetails(json) {
     console.dir(json);
+
+    const image = document.querySelector(".image");
+    const heading = document.querySelector("h1");
+    const description = document.querySelector(".description");
+    const newBackground = json.background_image;
+
+    image.style = `background-image: url('${newBackground}')`;
+    heading.innerHTML = json.name;
+    description.innerHTML = json.description;
 }
 
 function handleErrors(errors) {
